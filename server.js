f5080e280483d002370cdc99c7613d31556cb9b1
jerem <jeremy.tellaa@tanker.io>
2019-11-19 15:25:08 +0100

    feat(server): random identity

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const uuid = require('uuid/v4');
+const tanker = require('@tanker/identity');
 
 
 const app = express();
@@ -9,6 +11,14 @@ const appSecret = 'YOUR_APP_SECRET';
 
 app.use(bodyParser.json());
 
-app.post('/identity', (req, res) => res.send('Hello, you are on the identity route'));
+app.post('/identity', async (req, res) => {
+  const id = uuid();
+  const tankerIdentity = await tanker.createIdentity(appID, appSecret, id);
+  const user = {
+    id,
+    tankerIdentity,
+  }
+  res.json(user);
+});
 
 app.listen(port, () => console.log(`Identity server listening on port ${port}!`));
